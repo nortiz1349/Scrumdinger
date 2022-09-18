f965c2c3f1460f255c0d8868d814de8479a26d22
Nortiz <nortiz1349@gmail.com>
2022-09-18 23:32:22 +0900

    Persisting data

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ScrumsView: View {
 	@Binding var scrums: [DailyScrum]
+	@Environment(\.scenePhase) private var scenePhase
 	@State private var isPresentingNewScrumView = false
 	@State private var newScrumData = DailyScrum.Data()
+	let saveAction: ()->Void
 	
     var body: some View {
 		List {
@@ -51,6 +53,9 @@ struct ScrumsView: View {
 					}
 			}
 		}
+		.onChange(of: scenePhase) { phase in
+			if phase == .inactive { saveAction() }
+		}
     }
 }
 
@@ -58,7 +63,7 @@ struct ScrumsView_Previews: PreviewProvider {
 	
     static var previews: some View {
 		NavigationView {
-			ScrumsView(scrums: .constant(DailyScrum.sampleData))
+			ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
 		}
     }
 }
